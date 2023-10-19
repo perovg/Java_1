3b1510e498ff8fed52d387c6a90161f40cd6d04d
Grigory Perov <perov.gm@phystech.edu>
2023-10-19 23:24:25 +0300

    Homework3 1

---
+++ Homework3.java
@@ -0,0 +1,13 @@
+public class Homework3 {
+    public static void main(String[] args) {
+        //Task 1
+        /*IntSequence sequence = IntSequence.of(1, 2, 3, 4);
+        for (int i = 0; i < 5; i ++) {
+            System.out.println(Integer.toString(sequence.next()));
+        }*/
+        /*IntSequence constant = IntSequence.constant(3);
+        for (int i = 0; i < 4; i++) {
+            System.out.println(Integer.toString(constant.next()));
+        }*/
+    }
+}
