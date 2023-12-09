fe183f51360ca690f3c3770b3f4d82cf1981ca9e
Grigory Perov <perov.gm@phystech.edu>
2023-12-09 17:45:50 +0300

    Homework7 with Task 1

--- MultiplicationByTwo.java
+++ MultiplicationByTwo.java
@@ -1,14 +1,15 @@
-import java.util.function.BiFunction;
-import java.util.function.Function;
+import java.util.function.*;;
 
 public class MultiplicationByTwo {
+    public static void main(String[] args) {
+        BiFunction<Integer, Integer, Integer> multiply = (a, b) -> a * b;
+        int a = 2;
+        Function<Integer, Integer> multiplyBy2 = bind(multiply, a);
+        int result = multiplyBy2.apply(3);
+        System.out.println(result);
+    }
+    
     static <A, B, C> Function<B, C> bind(BiFunction<A, B, C> fn, A a) {
         return b -> fn.apply(a, b);
-    } 
-    public static void main(String[] args) {
-        Function<Integer, Integer> inc = bind(Integer::sum, 1);
-        System.out.println("inc: " + inc.apply(10) + "\n");
     }
-
-    
 }
